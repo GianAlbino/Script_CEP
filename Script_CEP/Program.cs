a14ed66f0e494aac753a9ed958437480cb5400e2
Gian Carlo Albino <giancarloalbino@hotmail.com>
2023-10-31 18:14:50 -0300

    Adicionar arquivos de projeto.

---
+++ Script_CEP/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Reflection;
+
+namespace Script_CEP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var csvLocation = CreateOrGetRootDirectory();
+            var insertHeader = true;
+
+            foreach (string csv in csvLocation)
+            {
+                using var reader = new StreamReader(csv);
+
+                var keyValuePairs = new Dictionary<string, List<string>>();
+
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(';');
+
+                    if (insertHeader)
+                    {
+                        foreach (var v in values)
+                        {
+                            keyValuePairs.Add(v, new List<string>());
+                        }
+                    }
+                    else
+                    {
+                        int i = 0;
+
+                        foreach (var v in values)
+                        {
+                            keyValuePairs.ElementAtOrDefault(i).Value.Add(v);
+                            i++;
+                        }
+                    }
+
+                    insertHeader = false;
+                }
+
+                InsertInDB(keyValuePairs);
+            }
+        }
+
+        private static async void InsertInDB(Dictionary<string, List<string>> keyValuePairs)
+        {
+            for (int i = keyValuePairs["Id"].Count - 1; i > 0; i--)
+            {
+                string candidateId = keyValuePairs["Id"][i];
+                string candidateZipCode = keyValuePairs["ZipCode"][i];
+
+                Console.WriteLine($"Atualizando candidate: {candidateId}, com zipCode: {candidateZipCode}");
+
+                await ServiceAPI
+                    .Instance()
+                    .UpdateCandidate(
+                    new Guid(candidateId),
+                    candidateZipCode);
+            }
+        }
+
+        private static List<string> CreateOrGetRootDirectory()
+        {
+            var rootDirectory = $"{Environment.CurrentDirectory}/Logs/";
+
+            if (!Directory.Exists(rootDirectory))
+            {
+                Directory.CreateDirectory(rootDirectory);
+            }
+
+            return Directory.GetFiles(rootDirectory).ToList();
+        }
+    }
+}
---
+++ Script_CEP/ServiceAPI.cs
@@ -0,0 +1,37 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Script_CEP
+{
+    public class ServiceAPI
+    {
+        private static ServiceAPI _service;
+
+        public static ServiceAPI Instance() 
+        { 
+            _service ??= new ServiceAPI();
+
+            return _service;
+        }
+
+        public async Task<bool> UpdateCandidate(Guid id, string zipCode)
+        {
+            var client = new RestClient("http://localhost:5012/api/");
+            var request = new RestRequest($"Candidate/UpdateZipCode?id={id}&zipCode={zipCode}");
+            var response = client.ExecuteGet(request);
+
+            Console.WriteLine(response.StatusCode);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
